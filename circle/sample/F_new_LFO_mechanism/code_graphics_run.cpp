560215f0e4426923718667c9ce128df06a72bfe7
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-09-03 18:16:13 +0200

    fix g_frameStart;

--- circle/sample/F_new_LFO_mechanism/code_graphics_run.cpp
+++ circle/sample/F_new_LFO_mechanism/code_graphics_run.cpp
@@ -241,7 +241,7 @@ void            CKernel::fpsBreak()
                 check();
 #endif
 #ifdef __DEBUG_TIMING__
-                g_glDuration = - g_frameStart // (m_Timer.GetClockTicks() - g_frameStart) - g_runtimeDuration;
+                g_glDuration = - g_frameStart; // (m_Timer.GetClockTicks() - g_frameStart) - g_runtimeDuration;
 #endif
                 g_frameCurrent = getClockMilliseconds(); // m_Timer.GetClockTicks();
                 g_frameTarget  = g_frameStart + (1000 / TARGET_FPS); // g_frameStart + (1000000 / TARGET_FPS);
