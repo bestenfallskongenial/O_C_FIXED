ea733c29a81c6cf76e2aa5e0928449eb9ba96555
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-09-06 20:30:06 +0200

    fix backbuffer

--- circle/sample/G-we_like_to_have_a_backbuffer/kernel_run.cpp
+++ circle/sample/G-we_like_to_have_a_backbuffer/kernel_run.cpp
@@ -36,14 +36,14 @@ TShutdownMode CKernel::Run(void)
                         }
                     if( m_SD_has_load && m_USB_has_load )
                         {
-                        saveFromBufferM         (   PARTITION_NAME_SD,
+                        saveFromBuffer         (   PARTITION_NAME_SD,
                                                   /*gen83FileName("TXT"*/
                                                     "bootlog.txt",
                                                     m_logKernel,            // stores the pre-init buffer
                                                     m_logKernelIndex );
 
                                                     msDelay(100);
-                        saveFromBufferM         (   PARTITION_NAME_SD,
+                        saveFromBuffer         (   PARTITION_NAME_SD,
                                                     "GLSL.txt",
                                                     m_bufferLog[1],
                                                     m_bufferLogIndex[1] );
