2809c49821f084aa29948ba85a62f694c1ce756d
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-09-10 21:01:06 +0200

    new folder new fun

---
+++ circle/sample/K_lets_get_serious_here/code_graphics_run.cpp
@@ -0,0 +1,340 @@
+#include "kernel.h"
+
+    #define MY_BFR   m_bufferLog[LOG_GLSL_0]                 // means the log goes into the pre-init buffer 
+    #define MY_IDX   m_bufferLogIndex[LOG_GLSL_0] 
+
+void            CKernel::frmBufferSet               (   vtx_state* v)
+{
+                glBindFramebuffer(GL_FRAMEBUFFER,0);
+
+                glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+                glBindBuffer(GL_ARRAY_BUFFER, v->gl_buf);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+}
+
+void            CKernel::frmBufferSwap              (   olg_state* o )
+{
+                eglSwapBuffers(o->display, o->surface);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+}
+
+void            CKernel::setUniPrg                  (   olg_state*  o, 
+                                                        glsl_state* s, 
+                                                        tex_state*  t,
+                                                    /*  int         gl_current_tex, */
+                                                        unsigned    p_validTextureCount )
+{
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif    
+                glUseProgram(s->gl_program_id[g_gl_program_current]);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+                if(s->u_time[g_gl_program_current] != -1)  glUniform1f(s->u_time[g_gl_program_current], GLtime);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif                
+                if(s->u_tres[g_gl_program_current]!= -1 )  glUniform2f(s->u_tres[g_gl_program_current], o->screen_width, o->screen_height);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif                
+                if(s->u_seed[g_gl_program_current] != -1)  glUniform4f(s->u_seed[g_gl_program_current], g_inOutMatrixFlt[0][RND], 
+                                                                                                        g_inOutMatrixFlt[1][RND], 
+                                                                                                        g_inOutMatrixFlt[2][RND], 
+                                                                                                        g_inOutMatrixFlt[3][RND]);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif                                                                                                        
+                if(s->u_aud[g_gl_program_current]!= -1 )   glUniform4f(s->u_aud[g_gl_program_current],  g_inOutMatrixFlt[0][AU0], 
+                                                                                                        g_inOutMatrixFlt[0][AU1], 
+                                                                                                        g_inOutMatrixFlt[0][AU2], 
+                                                                                                        g_inOutMatrixFlt[0][AU3]);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif                                                                                                        
+                if(s->u_col[g_gl_program_current] != -1)   glUniform4f(s->u_col[g_gl_program_current],  0.0f, 0.0f, 0.0f, g_opaque);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif                
+                if(s->u_par_a[g_gl_program_current] != -1) glUniform4f(s->u_par_a[g_gl_program_current],g_inOutMatrixFlt[0][OUT], 
+                                                                                                        g_inOutMatrixFlt[1][OUT], 
+                                                                                                        g_inOutMatrixFlt[2][OUT], 
+                                                                                                        g_inOutMatrixFlt[3][OUT]);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif                                                                                                        
+                if(s->u_par_b[g_gl_program_current] != -1) glUniform4f(s->u_par_b[g_gl_program_current],g_inOutMatrixFlt[4][OUT], 
+                                                                                                        g_inOutMatrixFlt[5][OUT], 
+                                                                                                        g_inOutMatrixFlt[6][OUT], 
+                                                                                                        g_inOutMatrixFlt[7][OUT]);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif                                                                                                        
+                if(s->u_tex_l[g_gl_program_current] != -1) glUniform1i(s->u_tex_l[g_gl_program_current],p_validTextureCount); 
+
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+}
+
+void            CKernel::setTexPrg                  (   olg_state*  o, 
+                                                        glsl_state* s, 
+                                                        tex_state*  t,
+                                                        int         gl_current_tex,
+                                                        unsigned    p_validTextureCount )
+{
+#ifdef __H264_DEBUG_TEX__
+                glActiveTexture(GL_TEXTURE0);
+                glBindTexture(GL_TEXTURE_2D, m_TextureA);
+
+                if (t->u_tex_id[g_gl_program_current][0] != -1) glUniform1i(t->u_tex_id[g_gl_program_current][0], 0);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif   
+
+#endif   
+
+#ifndef __H264_DEBUG_TEX__
+                if (g_centralModeBuffer[g_currentProgramBuffer][SEL_TEX] == FLAG_THRESHOLD)
+                    {
+                    for (unsigned i = 0; i < p_validTextureCount; i++)
+                        {
+                        glActiveTexture(GL_TEXTURE0+i);
+                        glBindTexture(GL_TEXTURE_2D, t->gl_tex_id[i]);
+
+                        if (t->u_tex_id[g_gl_program_current][i] != -1) glUniform1i(t->u_tex_id[g_gl_program_current][i], i);
+#ifdef __DEBUG_GL__
+                        debug_gl();
+#endif   
+                        }
+                    }
+                else
+                    {
+                    switch(p_validTextureCount)
+                        {
+                        case 0:
+
+                        break;
+
+                        case 1:
+                            glActiveTexture(GL_TEXTURE0);
+                            glBindTexture(GL_TEXTURE_2D, t->gl_tex_id[gl_current_tex]);
+
+                            if (t->u_tex_id[g_gl_program_current][0] != -1) glUniform1i(t->u_tex_id[g_gl_program_current][0], 0);
+#ifdef __DEBUG_GL__
+                            debug_gl();
+#endif   
+                        break;
+
+                        default:
+                            glActiveTexture(GL_TEXTURE0);
+                            glBindTexture(GL_TEXTURE_2D, t->gl_tex_id[gl_current_tex]);
+
+                            if (t->u_tex_id[g_gl_program_current][0] != -1) glUniform1i(t->u_tex_id[g_gl_program_current][0], 0);
+#ifdef __DEBUG_GL__
+                            debug_gl();
+#endif   
+                            glActiveTexture(GL_TEXTURE1);
+                            glBindTexture(GL_TEXTURE_2D, t->gl_tex_id[gl_current_tex + 1]);
+
+                            if (t->u_tex_id[g_gl_program_current][1] != -1) glUniform1i(t->u_tex_id[g_gl_program_current][1], 1);
+#ifdef __DEBUG_GL__
+                            debug_gl();
+#endif   
+                        break;
+                        }
+                    }
+#endif   
+}
+
+bool            CKernel::setTexBackbuffer           (   glsl_state* s,
+                                                        tex_state*  t,
+                                                        unsigned    p_validTextureCount )
+{
+                if (!t->gl_tex_bfr || t->u_tex_bfr[g_gl_program_current] == -1)
+                    {
+                    return false;
+                    }
+
+                GLint f_textureUnits = 0;
+                glGetIntegerv(GL_MAX_TEXTURE_IMAGE_UNITS, &f_textureUnits);
+
+                unsigned f_usedTextureUnits = 0;
+
+                if (g_centralModeBuffer[g_currentProgramBuffer][SEL_TEX] == FLAG_THRESHOLD)
+                    {
+                    f_usedTextureUnits = p_validTextureCount;
+                    }
+                else if (p_validTextureCount == 1)
+                    {
+                    f_usedTextureUnits = 1;
+                    }
+                else if (p_validTextureCount > 1)
+                    {
+                    f_usedTextureUnits = 2;
+                    }
+
+                if (f_usedTextureUnits >= (unsigned)f_textureUnits)
+                    {
+                    return false;
+                    }
+
+                glActiveTexture(GL_TEXTURE0 + f_usedTextureUnits);
+                glBindTexture(GL_TEXTURE_2D, t->gl_tex_bfr);
+
+                glUniform1i(t->u_tex_bfr[g_gl_program_current], f_usedTextureUnits);
+
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+
+                return true;
+}
+
+void            CKernel::captureBackbuffer          (   olg_state*  o,
+                                                        tex_state*  t )
+{
+                if (!t->gl_tex_bfr)
+                    {
+                    return;
+                    }
+
+                glBindTexture(GL_TEXTURE_2D, t->gl_tex_bfr);
+
+                glCopyTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, 0, 0,
+                                    o->screen_width, o->screen_height);
+
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+}
+
+
+void            CKernel::drawGLsPrg                 (   )
+{
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif    
+                glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+                glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+/*
+void            CKernel::frmRateBreak               (   bool noTargetFPS )
+{
+                glFlush();
+
+                if ( noTargetFPS )
+                    {
+                    glFinish();
+#ifdef __DEBUG_GL__
+                    debug_gl();
+#endif
+                    }
+}
+*/
+void            CKernel::setUniOvl                  (   olg_state*  o, 
+                                                        glsl_state* s, 
+                                                        tex_state*  t )
+{
+                glUseProgram(s->gl_program_id[0]);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+                if (s->u_tile_count[0] != -1) glUniform1i(s->u_tile_count[0], MENU_GPU_TILE_COUNT);
+
+                if (s->u_tile_rect[0] != -1) glUniform4fv(s->u_tile_rect[0], MENU_GPU_TILE_COUNT, s->tile_rect);
+
+                if (s->u_tile_index[0] != -1) glUniform1fv(s->u_tile_index[0], MENU_GPU_TILE_COUNT, s->tile_index);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+}
+
+void            CKernel::setTexOvl                  (   olg_state*  o, 
+                                                        glsl_state* s, 
+                                                        tex_state*  t )
+{
+                glActiveTexture(GL_TEXTURE0);
+                glBindTexture(GL_TEXTURE_2D, t->gl_tex_id[0]);
+
+                if (s->u_atlas[0] != -1) glUniform1i(s->u_atlas[0], 0);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+}
+
+void            CKernel::drawGLsOvl                 (   )
+{
+                glEnable(GL_BLEND);
+                glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            //  glBlendFunc(GL_ZERO, GL_SRC_COLOR);                 // shall fix the "no alfa in my 24bit bmp texture bitmap" issue
+                glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+                glDisable(GL_BLEND);
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+}
+
+// lets try the fps break here:
+
+void            CKernel::fpsBegin()
+{
+                g_frameStart = getClockMilliseconds(); // m_Timer.GetClockTicks();
+}
+
+void            CKernel::fpsBreak()
+{
+#ifdef __DEBUG_TIMING__    
+                g_runtimeDuration = getClockMilliseconds() - g_frameStart; //m_Timer.GetClockTicks() - g_frameStart;
+#endif                
+                glFlush();
+#ifdef __DEBUG_GL__
+                debug_gl();
+#endif
+#ifdef __DEBUG_TIMING__
+            //  g_glDuration = - g_frameStart; // (m_Timer.GetClockTicks() - g_frameStart) - g_runtimeDuration;
+                g_glDuration = (getClockMilliseconds() - g_frameStart) - g_runtimeDuration;                
+#endif
+                g_frameCurrent = getClockMilliseconds(); // m_Timer.GetClockTicks();
+                g_frameTarget  = g_frameStart + (1000 / TARGET_FPS); // g_frameStart + (1000000 / TARGET_FPS);
+
+                if (g_limitFPS && g_frameTarget > g_frameCurrent + g_lastSwapDuration)
+                    {
+                    g_frameDelay = g_frameTarget - (g_frameCurrent + g_lastSwapDuration);
+
+                    msDelay(g_frameDelay);
+                    }
+
+                g_frameCurrent = getClockMilliseconds(); // m_Timer.GetClockTicks();
+}
+
+void            CKernel::fpsEnd()
+{
+                g_frameEnd = getClockMilliseconds(); // m_Timer.GetClockTicks();
+
+                g_lastSwapDuration = g_frameEnd - g_frameCurrent;
+
+#ifdef __DEBUG_TIMING__
+                g_glDuration += g_lastSwapDuration;
+#endif
+
+                g_frameTime        = g_frameEnd - g_frameStart;
+
+                if (g_frameTime) g_currentFPS = 1000.0f / g_frameTime; // 1000000.0f / g_frameTime;
+}
+
+// END OF FILE
+
+
---
+++ circle/sample/K_lets_get_serious_here/kernel_run.cpp
@@ -0,0 +1,133 @@
+#include "kernel.h"
+
+    #define MY_BFR   m_logKernel                 // means the log goes into the pre-init buffer 
+    #define MY_IDX   m_logKernelIndex 
+
+TShutdownMode CKernel::Run(void)
+{
+                while (/*m_resetFlag == false*/ 1)
+                    {
+
+                    m_CPUThrottle.Update(); // NEW!!!
+                       
+                    if (!m_SD_has_load)                                             // first load block - get the system files from sd
+                        {
+                        wrapper_load_sd();
+                        wrapper_parser_sd();
+                        wrapper_init_gl_sd();
+
+                    //  msDelay(3000);
+                    //  m_logKernelIndex = 0;
+                        bufferScreenClear();
+
+                        m_SD_has_load = true;
+                        }
+                    if (updateUSB("umsd1") == true && m_USB_has_load == false)      // second load block - get user files from usb AND there is a devide attached
+                        {
+                        wrapper_load_usb();   
+                        wrapper_parser_usb();
+                        wrapper_init_gl_usb();
+
+                    //  msDelay(3000);
+                    //  m_logKernelIndex = 0;
+                        bufferScreenClear();
+
+                        m_USB_has_load = true;
+                        }
+                    if( m_SD_has_load && m_USB_has_load && !m_bootLogsSaved )
+                //  if( m_SD_has_load && m_USB_has_load )
+                        {
+                        m_bootLogsSaved = true;
+                        
+                        saveFromBuffer         (   PARTITION_NAME_SD,
+                                                  /*gen83FileName("TXT"*/
+                                                    "bootlog.txt",
+                                                    m_logKernel,            // stores the pre-init buffer
+                                                    m_logKernelIndex );
+
+                        saveFromBuffer         (   PARTITION_NAME_SD,
+                                                    "GLSL.txt",
+                                                    m_bufferLog[LOG_GLSL_0],
+                                                    m_bufferLogIndex[LOG_GLSL_0] );          
+
+                        m_logKernelIndex = 0;
+                        bufferScreenClear();
+                        } 
+
+                fpsBegin();                                                             // here starts the actual runtimeloop
+
+                    updateMIDI();
+                  
+                    resetMenuPickUpFlags();
+                    storeModes();
+
+                //  m_audio_hold_A = AUDIO_HOLD_TIMEOUT;                            // to debug the audio mode and menu code!!!
+                //  m_audio_hold_B = AUDIO_HOLD_TIMEOUT;
+                    
+                    readAndConvertADC();
+             
+                    adc_ProcessAudio();
+
+                    adc_AdvanceIndex();
+
+                    randomVec8(g_frameStart);
+
+                    buttonPingB( 0, SW_PIN_A, 1, SW_PIN_B );                        // 
+
+                    buttonConsumer(0,1);                                              //  here goes the button consumer and menu code
+
+                    dispatchLayer();
+
+                    applyTargetModes();  // <- correct place here?
+
+                    getChannelModeA(0);
+                    getChannelModeA(1);
+                    getChannelModeA(2);
+                    getChannelModeA(3);
+                    getChannelModeA(4);
+                    getChannelModeA(5);
+                    getChannelModeA(6);
+                    getChannelModeA(7);
+
+                    menuLedUpdate();
+
+                    sample1WaveTable( m_bufferLfo, 0, LF1_WAVE, LF1, 1023 );
+                    sample1WaveTable( m_bufferLfo, 1, LF2_WAVE, LF2, 1023 );                    
+
+                    predict1Beat( 0, LF1_MULT );
+                    predict1Beat( 1, LF2_MULT ); 
+                                       
+                    logModesRuntime( 0 );
+
+                    logInfosRuntime( 10 );
+
+                    logInfoFrameRate( 20 );
+
+                    setUniPrg(&m_ogl,
+                            &m_fsh,
+                            &m_tex,
+                            filecounter[FT_FSH][FLD_VALID]);
+                            
+                    setTexPrg(&m_ogl,
+                            &m_fsh,
+                            &m_tex,
+                            m_activeTex,
+                            filecounter[FT_TEX][FLD_VALID]);                            
+
+                //  bool f_backbuffer = setTexBackbuffer( &m_fsh, &m_tex, filecounter[FT_TEX][FLD_VALID]);
+                    setTexBackbuffer( &m_fsh, &m_tex, filecounter[FT_TEX][FLD_VALID]);
+
+                    drawGLsPrg();
+
+                //  if (f_backbuffer) captureBackbuffer( &m_ogl, &m_tex );
+                    captureBackbuffer( &m_ogl, &m_tex );
+                      
+                fpsBreak();
+
+                    frmBufferSwap(&m_ogl); 
+
+                fpsEnd();
+
+                    }
+                return ShutdownHalt;                    
+}
