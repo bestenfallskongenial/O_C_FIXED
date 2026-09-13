5d4a96c0bcf809107e703ab3cb3aa8f6dec2d7d9
bestenfallskongenial <77353739+bestenfallskongenial@users.noreply.github.com>
2026-09-13 19:34:09 +0200

    mmhhh

--- circle/sample/K_lets_get_serious_here/kernel_run.cpp
+++ circle/sample/K_lets_get_serious_here/kernel_run.cpp
@@ -80,8 +80,8 @@ TShutdownMode CKernel::Run(void)
 
                     applyTargetModes();  // <- correct place here?
 
-                //  getChannelModeB();
-
+                    getChannelModeB();
+/*
                     getChannelModeA(0);
                     getChannelModeA(1);
                     getChannelModeA(2);
@@ -90,7 +90,7 @@ TShutdownMode CKernel::Run(void)
                     getChannelModeA(5);
                     getChannelModeA(6);
                     getChannelModeA(7);
-
+*/
                     menuLedUpdate();
 
                     sample1WaveTable( m_bufferLfo, 0, LF1_WAVE, LF1, 1023 );
